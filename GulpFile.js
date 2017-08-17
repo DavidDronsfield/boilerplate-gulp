8374d6ffea1413b674696652d554c66bbebd6130
David Dronsfield <David.Dronsfield@inntel.co.uk>
2017-08-17 15:10:08 +0100

    more features

--- GulpFile.js
+++ GulpFile.js
@@ -1,16 +1,25 @@
 'use strict';
 
 const gulp = require('gulp');
+// const browserSync = require('browser-sync').create();	Commented out until required
+
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const sassdoc = require('sassdoc');
 const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
-// const browserSync = require('browser-sync').create();	Commented out until required
+const newer = require('gulp-newer');
+const imageMin = require('gulp-imagemin');
+// const responsive = require('gulp-responsive-images');	Commented out until required
 
-const input = './assets/sass/*.scss';
-const output = './public/css';
+const cleanedFiles = [ 'public/css/*.css' ];
+const stylesInput = './assets/sass/*.scss';
+const stylesOutput = './public/css';
+const imageInput = './assets/images/**/*';
+const imageOutput = './public/images';
+const htmlInput = './views/*.html';
+const htmlOutput = './public/html';
 
 const sassOptions = {
   errLogToConsole: true,
@@ -22,9 +31,15 @@ const autoprefixerOptions = {
   cascade: false
 };
 
-const sassdocOptions = {
-  dest: './public/sassdoc'
-};
+const sassdocOptions = {  dest: './public/sassdoc' };
+
+const imageOptions = [
+    imageMin.gifsicle({interlaced: true}),
+    imageMin.jpegtran({progressive: true}),
+    imageMin.optipng({optimizationLevel: 5}),
+    imageMin.svgo({plugins: [{removeViewBox: true}]})
+];
+
 
 // gulp.task('browser-sync', () => {
 // 	browserSync.init({
@@ -35,25 +50,56 @@ const sassdocOptions = {
 // });
 
 gulp.task('clean', () => {
-	return del([
-		'public/css/*.css'
-	]);
+	return del(cleanedFiles);
 });
 
-gulp.task('sass', [ 'clean' ], () => {
+gulp.task('optimiseImages', () => {
 	return gulp
-		.src(input)
+		.src(imageInput)
+		.pipe(newer(imageOutput))
+		.pipe(imageMin(imageOptions))
+		.pipe(gulp.dest(imageOutput));
+});
+
+// gulp.task('optimiseBackgrounds', () => {
+// 	return gulp
+// 		.src(imageInput)
+// 		.pipe(responsive({
+// 			'*': [
+// 			{
+// 				width: 3500,
+// 				rename: { suffix: '-full' }
+// 			}, {
+// 				width: 1920,
+// 				rename: { suffix: '-desktop' }
+// 			}, {
+// 				width: 1024,
+// 				rename: { suffix: '-laptop' }
+// 			}, {
+// 				width: 768,
+// 				rename: { suffix: '-tablet' }
+// 			}, {
+// 				width: 425,
+// 				rename: { suffix: '-mobile' }
+// 			}
+// 		]}, imageOptions))
+// 		.pipe(gulp.dest(imageOutput));
+// });
+
+gulp.task('sass', [ 'clean', 'optimiseImages' ], () => {
+	return gulp
+		.src(stylesInput)
 		.pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(autoprefixer(autoprefixerOptions))
 		.pipe(cssnano())
-		.pipe(gulp.dest(output))
+		.pipe(gulp.dest(stylesOutput))
 		.resume();
 });
 
 gulp.task('watch', () => {
 	return gulp
-		.watch(input, ['sass'])
+		.watch(stylesInput, ['sass'])
 		.on('change', (event) => {
 			console.log(`File ${event.path} was ${event.type}, running tasks...`)
 		});
@@ -61,7 +107,7 @@ gulp.task('watch', () => {
 
 gulp.task('sassdoc', () => {
 	return gulp
-		.src(input)
+		.src(stylesInput)
 		.pipe(sassdoc(sassdocOptions))
 		.resume();
 });
